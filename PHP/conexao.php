0fcc798f81f76f19862b5254605368cab7897795
Luidy Wertuly <127540434+LuidyWertuly@users.noreply.github.com>
2024-06-13 09:33:16 -0300

    Update conexao.php

--- PHP/conexao.php
+++ PHP/conexao.php
@@ -1,10 +1,5 @@
 <?php
 
-// $dbHost = 'localhost';
-// $dbUsername = 'apunat60_admin';
-// $dbPassword = 'T@s23106672';
-// $dbName = 'apunat60_Projeto-LWs';
-
 $dbHost = 'localhost';
 $dbUsername = 'root';
 $dbPassword = '';
